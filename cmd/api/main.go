6b3df238792b7bc629b7518ec576966455435b4a
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-04-28 21:01:47 +0300

    Add routing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,46 @@
 package api
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"rest-api/internal/database"
+	"rest-api/internal/database/repositories"
+	"rest-api/internal/handlers"
+)
 
+func main() {
+	dbUrl := "postgres://postgres:5432@localhost:5432/RestApi"
+
+	db, err := database.Connect(dbUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	taskRepo := repositories.NewTaskRepo(db)
+	taskHandler := handlers.NewTaskHandler(taskRepo)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/tasks", methodHandler(taskHandler.GetAll, http.MethodGet))
+	mux.HandleFunc("/tasks/", methodHandler(taskHandler.GetById, http.MethodGet))
+
+	err = http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func methodHandler(handlerFunc http.HandlerFunc, allowedMethod string) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != allowedMethod {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		handlerFunc(w, req)
+	}
 }
