eea8fb14e3d1a8d00e03f7f02dd180c8444d4e33
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-04-28 20:35:54 +0300

    Add TaskHandler

---
+++ internal/handlers/task.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"rest-api/internal/database/repositories"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+type TaskHandler struct {
+	repo *repositories.TaskRepo
+}
+
+func NewTaskHandler(repo *repositories.TaskRepo) *TaskHandler {
+	return &TaskHandler{repo: repo}
+}
+
+func respondWithJson(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(payload)
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, message string) {
+	respondWithJson(w, statusCode, map[string]string{"error": message})
+}
+
+func (h *TaskHandler) GetAll(w http.ResponseWriter, req *http.Request) {
+	tasks, err := h.repo.GetAll()
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to retrieve tasks")
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, tasks)
+}
+
+func (h *TaskHandler) GetById(w http.ResponseWriter, req *http.Request) {
+	pathParts := strings.Split(strings.TrimPrefix(req.URL.RawPath, "/tasks/"), "/")
+	idStr := pathParts[0]
+
+	id, err := uuid.Parse(idStr)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid task ID")
+		return
+	}
+
+	task, err := h.repo.GetById(id)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to retrieve task")
+		return
+	}
+
+	if task == nil {
+		respondWithError(w, http.StatusNotFound, "Task not found")
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, task)
+}
